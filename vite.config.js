d9d205ea7bb7e11d742c088889569828c291806b
Noah Park-Nguyen <noahparknguyen@gmail.com>
2025-01-14 11:57:13 -0500

    updated paths

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-2025/",
+  base: "/Portfolio-V2/",
+  resolve: { extensions: [".js", ".ts", ".tsx", ".jsx"] },
 });
